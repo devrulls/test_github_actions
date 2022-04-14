06c40a8a8710d4442024a265d613d044edc3177c
Raul Hernandez <devrulls@gmail.com>
2022-04-14 12:19:58 +0200

    test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from main import add, divide, multiplication
 
 
 def test_add_two_numbers():
-    assert add(5, 10) == 15
+    assert add(5, 12) == 17
 
 
 def test_divide_two_numbers():
@@ -11,7 +11,7 @@ def test_divide_two_numbers():
 
 
 def test_multiplication_two_numbers():
-    assert multiplication(30, 0) == 0
+    assert multiplication(30, 10) == 300
 
 
 def test_add_two_none():
